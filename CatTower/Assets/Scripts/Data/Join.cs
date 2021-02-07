7446fca5e0fb7486255293cd29249dcb9052f5bc
minho9710 <minho9710@naver.com>
2021-02-07 20:28:42 +0900

    title3.5
    get/active발송까지는 성공
    이후 방목록뽑는것과 추가수정필요
    join은 만들던중이라 더미로남김

---
+++ CatTower/Assets/Scripts/Data/Join.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CatTower
+{
+    [Serializable]
+    public class JoinRequest
+    {
+        public string roomId;
+        public class userInfo
+        {
+            public string nickname;
+            public string mid;
+        }
+    }
+
+    [Serializable]
+    public class JoinResponse
+    {
+            //userlist미구현
+    }
+}
