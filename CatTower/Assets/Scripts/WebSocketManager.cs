ca75f39d30cb55f8f605c9aa87ee33de5e295432
SeungYeon Kim <47077179+seunggom@users.noreply.github.com>
2021-02-15 01:48:11 +0900

    WebSocket 통신 가능한 코드 추가

---
+++ CatTower/Assets/Scripts/WebSocketManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using socket.io;
+using System;
+
+namespace CatTower
+{
+    public class WebSocketManager : SingletonGameObject<WebSocketManager>
+    {
+        private string _url = "http://localhost:8005";
+        //private Socket socket;
+        private Dictionary<string, Socket> socketList = new Dictionary<string, Socket>();
+
+        /// <summary>
+        /// 웹소켓 통신을 요청합니다
+        /// </summary>
+        /// <param name="_namespace">네임스페이스 (`/rooms` or `/ingame`)</param>
+        /// <param name="connectedAction">서버와 연결되었을 때 수행할 액션</param>
+        public bool Connect(string _namespace, Action connectedAction = null)
+        {
+            Socket socket = Socket.Connect(_url + _namespace);
+            socketList.Add(_namespace, socket);
+            if (connectedAction != null)
+            {
+                socket.On(SystemEvents.connect, connectedAction);
+            }
+            return socket != null;
+        }
+
+        /// <summary>
+        /// TODO: 수정할 예정
+        /// </summary>
+        /// <param name="_namespace"></param>
+        /// <returns></returns>
+        public bool Disconnect(string _namespace)
+        {
+            Socket socket;
+            socketList.TryGetValue(_namespace, out socket);
+            if (!socketList.TryGetValue(_namespace, out socket)) return false;
+            socketList.Remove(_namespace);
+            socket.Emit("disconnect");
+            socket.Disconnect();
+            return true;
+        }
+
+        /// <summary>
+        /// 게임 서버에 이벤트를 보냅니다
+        /// </summary>
+        /// <param name="_namespace"></param>
+        /// <param name="eventName"></param>
+        /// <param name="request"></param>
+        /// <typeparam name="TRequest"></typeparam>
+        /// <returns></returns>
+        public bool SendEvent<TRequest>(string _namespace, string eventName, TRequest request)
+        {
+            Socket socket;
+            socketList.TryGetValue(_namespace, out socket);
+            if (!socketList.TryGetValue(_namespace, out socket)) return false;
+
+            var requestToJson = JsonUtility.ToJson(request);
+            Debug.Log("request data:\n" + requestToJson);
+            socket.Emit(eventName, requestToJson.Replace("\"", "'"));
+            return true;
+        }
+
+        /// <summary>
+        /// 게임 서버로부터 이벤트를 받을 준비를 합니다
+        /// </summary>
+        /// <param name="_namespace"></param>
+        /// <param name="eventName"></param>
+        /// <param name="responseHandler"></param>
+        /// <typeparam name="TResponse"></typeparam>
+        /// <returns></returns>
+        public bool ReceiveEvent<TResponse>(string _namespace, string eventName, Action<TResponse> responseHandler)
+        {
+            Socket socket;
+            socketList.TryGetValue(_namespace, out socket);
+            if (!socketList.TryGetValue(_namespace, out socket)) return false;
+            socket.On(eventName, (string response) =>
+            {
+                Debug.Log("response data:\n" + response);
+                var change = response.Substring(1, response.Length - 2).Replace("\\", "");
+                Debug.Log(change);
+                TResponse responseObject = JsonUtility.FromJson<TResponse>(change);
+                if (responseHandler != null) responseHandler(responseObject);
+            });
+            return true;
+        }
+
+        /// <summary>
+        /// 해당 이벤트에 대한 콜백을 더이상 받지 않도록 합니다
+        /// </summary>
+        /// <param name="_namespace"></param>
+        /// <param name="eventName"></param>
+        /// <returns></returns>
+        public bool CancelToReceiveEvent(string _namespace, string eventName)
+        {
+            Socket socket;
+            socketList.TryGetValue(_namespace, out socket);
+            if (!socketList.TryGetValue(_namespace, out socket)) return false;
+
+            socket.Off(eventName);
+            return true;
+        }
+    }
+}
+
+// 출처: https://github.com/nhn/socket.io-client-unity3d
---
+++ CatTower/Assets/Scripts/test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatTower
+{
+    public class test : MonoBehaviour
+    {
+        WebSocketManager webSocket;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+             WebSocketManager.Instance.Connect("/rooms", () =>
+            {
+                // 진짜 예제이므로 UserListResponse는 쓰지마세요
+                // /rooms 네임스페이스에 속한 userlist 라는 이벤트를 받겠다라고 선언하는 것임
+                WebSocketManager.Instance.ReceiveEvent<UserListResponse>("/rooms", "userlist", null);
+            });
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+    [System.Serializable]
+    /// <summary>
+    /// 예제용으로 넣은거니 쓰지마세요
+    /// </summary>
+    public class UserListResponse
+    {
+        public string userlist;
+    }
+}
