afbee48be9a8fb8a279179143e70cf38901c5921
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2021-02-21 20:23:30 +0900

    02/21

---
+++ Assets/Scripts/Userlist_Refresh.cs
@@ -0,0 +1,36 @@
+﻿/*using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace CatTower{
+public class Userlist_Refresh : MonoBehaviour
+{
+    public Text nickname;
+    public Text mid;
+
+    List<string> user = new List<string>();
+
+    // Use this for initialization
+    void Start()
+    {
+        WebSocketManager.socket.On(" ", (data) =>
+        {
+            Debug.Log(data.Json.args[0]);
+            buffer.Add(data.Json.args[0].ToString());
+        });
+    }
+
+    void Update()
+    {
+        if (user.Count <= 0) return;
+
+        foreach (var b in user)
+        {
+            mid.text += b + "\n";
+        }
+        user.Clear();
+        }
+    
+    }
+}*/
