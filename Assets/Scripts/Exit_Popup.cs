8b844ed95c9735c6a2b6b24c22be92de1c14af06
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2021-02-21 20:38:59 +0900

    02/21

---
+++ Assets/Scripts/Exit_Popup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CatTower
+{
+    public class Exit_Popup : MonoBehaviour
+    {
+        public GameObject ExitPop;
+     
+
+        public void Exitpopupenable()
+        {
+            ExitPop.SetActive(true);
+        }
+
+        public void No()
+        {
+
+            ExitPop.SetActive(false);
+        }
+
+        public void Yes()
+        {
+            SceneManager.LoadScene("Title");
+        }
+
+    }
+}
